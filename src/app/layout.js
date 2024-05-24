5070a3224a42f33628d81fb82104a8f61269ba91
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-05-24 21:16:16 +0700

    Removed trivial authorization implementation using flags in local Storage. Added parallel routes for improved performance.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,51 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter, Roboto} from 'next/font/google'
 import React from "react";
 import Header from "@/components/header";
-import {AuthProvider} from "@/components/authProvider";
 
 const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin', 'cyrillic'], weight: ['100', '300', '400', '500', '700', '900'] })
 
 export const metadata = {
   title: 'GWALT, Best way to listen',
   description: 'Listen to music for your pleasure',
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, leftSide, rightSide }) {
   return (
-      <AuthProvider>
-        <html lang="en">
-          <body className={inter.className}>
-            <Header/>
-            <main className='min-h-screen max-w-[1140px] mx-auto my-[-2px]'>{children}</main>
-            <footer className='flex justify-center mt-9'>
-              <p>Designed by Grisha</p>
-            </footer>
-          </body>
-        </html>
-      </AuthProvider>
+      <html lang="en">
+        <body className={ inter.className }>
+        <Header/>
+        <main className='min-h-screen max-w-[1360px] mx-auto my-[-2px] mb-6'>
+          <div className="container max-w-7xl mx-auto px-8">
+            {children}
+            <div className="grid grid-cols-[auto_350px] gap-4 mt-[380px]">
+              <div className="overflow-hidden">
+                {leftSide}
+              </div>
+              <div className=''>
+                <div className='sticky top-6 right-0'>
+                  {rightSide}
+                  <div className="text-gray-500 text-sm border-t border-t-gray-100 pt-2 mt-6">
+                    <a href="#" className="hover:underline">Legal</a> -
+                    <a href="#" className="hover:underline">Privacy</a> -
+                    <a href="#" className="hover:underline">Cookie Policy</a> -
+                    <a href="#" className="hover:underline">Cookie Manager</a> -
+                    <a href="#" className="hover:underline">Imprint</a> -
+                    <a href="#" className="hover:underline">Artist Resources</a> -
+                    <a href="#" className="hover:underline">Blog</a> -
+                    <a href="#" className="hover:underline">Charts</a> -
+                    <br/>
+                    <span className="text-gray-700">
+                    Language: <a href="#" className="text-blue-600 hover:underline">English (US)</a>
+                  </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+        </body>
+      </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,22 @@
-import ArtistCard from "@/components/artistCard";
-import React from "react";
-
-const getArtists = async () => {
-    const response = await fetch('http://localhost:3000/api/users/?genres=true&tracksCount=true&likesCount=true&playsCount=true', { cache: 'no-cache' })
-
-    return await response.json();
-}
-
 export default async function Home() {
-    const artists = await getArtists()
+
     return(
-        <div className='flex flex-col items-center'>
-            <div
-                className='w-full flex items-center justify-center absolute h-[360px] p-8'
-                style={{
-                    backgroundImage: `url(https://images.wallpaperscraft.ru/image/single/el_derevo_siluet_185231_3840x2400.jpg)`,
-                    objectFit: 'cover',
-                    backgroundPosition: 'center',
-                }}
-            >
-                <div className='flex flex-col items-center gap-10'>
-                    <h1 className='text-3xl text-white bg-black bg-opacity-80 px-2 py-1'>Добро пожаловать на площадку gWALT</h1>
-                    <h1 className='text-xl text-white text-wrap w-[825px] text-center bg-black bg-opacity-80 px-2 py-1'>
-                        Слушайте треки своих любимых исполнителей и получайте удовольствие.
-                        Ничего лишнего, только вы и музыка
-                    </h1>
-                </div>
-            </div>
-            <div className='flex flex-col items-center gap-8 mt-[390px] w-full'>
-                <h2 className='text-2xl font-medium w-full text-center border-b-[1px] border-solid border-b-[#f2] pb-5'>Популярные исполнители</h2>
-                {artists.reverse().map(a => <ArtistCard id={a.UserID} name={a.Username} imageUrl={a.AvatarUrl} genres={a.Genres} description={a.Description} tracksCount={a.TracksCount} likesCount={a.LikesCount} playsCount={a.PlaysCount}/>)}
+        <div
+            className='w-full flex items-center justify-center absolute left-0 top-[43px] h-[360px] p-8'
+            style={{
+                backgroundImage: `url(https://images.wallpaperscraft.ru/image/single/el_derevo_siluet_185231_3840x2400.jpg)`,
+                objectFit: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <div className='flex flex-col items-center gap-10'>
+                <h1 className='text-3xl text-white bg-black bg-opacity-80 px-2 py-1'>
+                    Добро пожаловать на площадку gWALT
+                </h1>
+                <h1 className='text-xl text-white text-wrap w-[825px] text-center bg-black bg-opacity-80 px-2 py-1'>
+                    Слушайте треки своих любимых исполнителей и получайте удовольствие.
+                    Ничего лишнего, только вы и музыка
+                </h1>
             </div>
         </div>
     )
