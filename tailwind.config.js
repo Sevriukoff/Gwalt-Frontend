f6988ecc59d420fdd33cc0aca416c2997fc4fea7
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-05-24 21:04:01 +0700

    Updated Tailwind configuration to implement desktop-first design.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: false,
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      lg: { max: '1999.99px'},
+      md: {max: '991.99px'},
+      sm: {max: '767.99px'},
+      xs: {max: '479.99px'}
+    },
     extend: {
+      colors: {
+
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
