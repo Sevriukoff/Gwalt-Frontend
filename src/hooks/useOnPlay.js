7897a4ca4a8e12695b9b87eb2a845d5b603c025a
Sevriukoff <igor.sevriukoff@yandex.ru>
2024-06-23 17:57:24 +0700

    Enhanced useOnPlay() hook to support array of track IDs as input parameter.

--- src/hooks/useOnPlay.js
+++ src/hooks/useOnPlay.js
@@ -1,14 +1,17 @@
-import useMusicPlayer from "@/hooks/useMusicPlayer";
+import useMusicPlayer from '@/hooks/useMusicPlayer';
+import { useCallback } from 'react';
 
 const useOnPlay = (tracks = []) => {
-  const musicPlayerStore = useMusicPlayer();
+  const setId = useMusicPlayer((state) => state.setId);
+  const setIds = useMusicPlayer((state) => state.setIds);
 
-  const onPlay = (id) => {
-    musicPlayerStore.setId(id);
-    musicPlayerStore.setIds(tracks.map((t) => t.id));
-  }
+  const onPlay = useCallback((id) => {
+    const ids = tracks.map((t) => (typeof t === 'object' ? t.id : t));
+    setId(id);
+    setIds(ids);
+  }, [setId, setIds, tracks]);
 
   return onPlay;
-}
+};
 
 export default useOnPlay;
